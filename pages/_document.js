81b6e7b833f7b58b9a0cad540afcc8cf99bac555
Zayn Yusuf <yusue786@gmail.com>
2024-08-27 13:23:43 +0600

    adding icon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="favicon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
