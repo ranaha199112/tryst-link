72b4b3b907d8b0f3a52cebcb869d028053ed43bc
Zayn Yusuf <yusue786@gmail.com>
2025-03-01 01:29:23 +0600

    fixx

--- pages/recovery-auth.js
+++ pages/recovery-auth.js
@@ -48,14 +48,14 @@ function RecoveryAuthPage() {
   return (
     <>
       <Header />
-      <div className="flex  justify-start min-h-screen bg-white">
+      <div className="flex pl-20 justify-start min-h-screen bg-white">
         <div className="w-full max-w-md">
           <div className="text-center mb-6">
             <h1 className="text-3xl font-bold text-gray-900">
               tryst <span className="text-gray-500">.link</span>
             </h1>
           </div>
-          <div className="bg-red-100 text-red-700 p-3 rounded-md mb-4 text-sm">
+          <div className="bg-red-100 text-red-700 p-3 rounded-md mb-4 text-sm w-full">
             You need to authenticate via an additional factor before continuing
           </div>
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">
@@ -65,16 +65,19 @@ function RecoveryAuthPage() {
             Enter the 6-digit code from the authenticator app on your phone or
             password manager.
           </p>
+          <p className="text-gray-800 mb-4 text-lg font-semibold">
+            Authentication Token
+          </p>
           <input
             type="text"
             value={code}
             onChange={(e) => setCode(e.target.value)}
-            className="w-full px-4 py-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none text-start text-2xl tracking-widest"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none text-start text-2xl tracking-widest"
             placeholder="_ _ _ _ _ _"
             maxLength="6"
           />
           <button
-            className="w-[30%] bg-red-600 text-white py-3 mt-4 rounded-md hover:bg-red-700 text-lg font-semibold"
+            className="w-[30%] bg-red-600 text-white py-2 mt-4 rounded-md hover:bg-red-700 text-lg font-semibold"
             onClick={handleSubmit}
           >
             Authenticate
